c06730196caa17917ebc801b69215aa8c4cb4b12
Lauro Cruz <cruzlauroiii@gmail.com>
2017-08-01 11:48:58 +0800

    commit build files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import presetFlow from "babel-preset-flow";
+import transformReactJSX from "babel-plugin-transform-react-jsx";
+import transformSyntaxJSX from "babel-plugin-syntax-jsx";
+import transformReactDisplayName from "babel-plugin-transform-react-display-name";
+
+export default function() {
+  return {
+    presets: [presetFlow],
+    plugins: [transformReactJSX, transformSyntaxJSX, transformReactDisplayName],
+  };
+}
